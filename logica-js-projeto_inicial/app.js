30732c8bc5822508a0e59e44ef6fe88c2f36cb02
katilla <katillatga@hotmail.com>
2025-02-27 16:59:07 -0300

    alterando número maximo para 50.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,5 +1,5 @@
 alert ("Boas vindas ao jogo do número secreto!");
-let numeroMaximo = 65;
+let numeroMaximo = 55;
 let numeroSecreto = parseInt (Math.random() * numeroMaximo +1);
 console.log (numeroSecreto);
 let chute;
